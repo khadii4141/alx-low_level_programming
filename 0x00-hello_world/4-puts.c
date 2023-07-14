a3b87816f65eca07abbfc590ac4506238d0f5d08
khadii4141 <khadii4141@gmail.com>
2023-07-14 09:57:34 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main..program that prints exactly the given function with a new line
+ * Return:0 ()
+ */
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
