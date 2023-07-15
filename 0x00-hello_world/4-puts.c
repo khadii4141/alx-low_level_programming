22136b94995ad08a78557f622176031bf40f8b23
khadii4141 <khadii4141@gmail.com>
2023-07-14 20:59:34 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main:program that prints a line with puts function
  * Return:0(success)
  */
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
