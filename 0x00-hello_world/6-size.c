fc415d9c77ad8d681a7c2932c256ebb732c4d495
khadii4141 <khadii4141@gmail.com>
2023-07-16 12:25:01 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
 printf("Size of a char:%zu byte(s)\n", sizeof(char));
 printf("Size of an int:%zu byte(s)\n", sizeof(int));
 printf("Size of a float:%zu byte(s)\n", sizeof(float));
-printf("Size of a double:%zu byte(s)\n", sizeof(double));
 printf("Size of long int:%zu byte(s)\n", sizeof(long));
 printf("Size of long long int:%zu byte(s)\n", sizeof(long long));
 return (0);
