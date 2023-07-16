689ccfdf5393625b9bbf19a8f05d22449b918568
khadii4141 <khadii4141@gmail.com>
2023-07-16 13:50:52 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * Return:Always 1 (success)
+ * return:always 1 (success)
  */
 int main(void)
 {
-write(and that piece of art is useful"\ - Dora Korpar, 2015-10-19\n);
-Return(1);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1);
 }
