d61f75cfcc4914b6bce94e42a0bf2af9d9e0c3cc
khadii4141 <khadii4141@gmail.com>
2023-08-03 06:45:43 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _print_rev_recursion -function that prints a string in reverse
+ * @s:string
+ */
+
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
+
+
